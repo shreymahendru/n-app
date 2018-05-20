668a5767db730886ba9d904e3107130d478277aa
nivinjoseph <nivinjoseph@outlook.com>
2018-05-20 16:49:19 -0400

    added image compression and inlining capabilities

--- webpack.config.ts
+++ webpack.config.ts
@@ -47,12 +47,46 @@ const moduleRules: Array<any> = [
         }]
     },
     {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: ["file-loader"]
+        test: /\.svg$/,
+        use: [
+            isDev ? "file-loader" : {
+                loader: "url-loader",
+                options: {
+                    limit: 9000,
+                    fallback: "file-loader"
+                }
+            }
+        ]
+    },
+    {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+            isDev ? "file-loader" : {
+                loader: "url-loader",
+                options: {
+                    limit: 9000,
+                    fallback: "file-loader"
+                }
+            },
+            {
+                loader: "image-webpack-loader",
+                options: {
+                    bypassOnDebug: true,
+                }
+            }
+        ]
     },
     {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
+        use: [
+            isDev ? "file-loader" : {
+                loader: "url-loader",
+                options: {
+                    limit: 9000,
+                    fallback: "file-loader"
+                }
+            }
+        ]
     },
     {
         test: /\.(html)$/,
