38b69d7b7748d8efc5a38bcc1f9d8efdc6a443a1
nivinjoseph <nivinjoseph@outlook.com>
2018-12-18 04:49:07 -0500

    PageViewModel enhancement

--- src/core/page-view-model.ts
+++ src/core/page-view-model.ts
@@ -1,10 +1,23 @@
 import { RouteArgs } from "./route-args";
 import { BaseViewModel } from "./base-view-model";
+import "@nivinjoseph/n-ext";
 
 
 // public
 export class PageViewModel extends BaseViewModel
 {
+    protected get currentRoute(): string
+    {
+        let route: string = this.ctx.$route ? this.ctx.$route.fullPath : null;
+        route = route ? route.trim() : null;
+        if (route)
+        {
+            while (route.endsWith("/") || route.endsWith("?"))
+                route = route.substr(0, route.length - 1).trimRight();
+        }
+        return route;   
+    }
+    
     protected get pathArgs(): Object
     {
         let routeArgs: RouteArgs = (<any>this).__routeArgs;
