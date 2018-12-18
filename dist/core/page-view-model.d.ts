f1bab3eafe14de4963734e69818770a382c75643
nivinjoseph <nivinjoseph@outlook.com>
2018-12-18 04:49:26 -0500

    preparing to publish new version

--- dist/core/page-view-model.d.ts
+++ dist/core/page-view-model.d.ts
@@ -1,5 +1,7 @@
 import { BaseViewModel } from "./base-view-model";
+import "@nivinjoseph/n-ext";
 export declare class PageViewModel extends BaseViewModel {
+    protected readonly currentRoute: string;
     protected readonly pathArgs: Object;
     protected readonly queryArgs: Object;
     protected onEnter(...params: any[]): void;
--- dist/core/page-view-model.js
+++ dist/core/page-view-model.js
@@ -1,7 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const base_view_model_1 = require("./base-view-model");
+require("@nivinjoseph/n-ext");
 class PageViewModel extends base_view_model_1.BaseViewModel {
+    get currentRoute() {
+        let route = this.ctx.$route ? this.ctx.$route.fullPath : null;
+        route = route ? route.trim() : null;
+        if (route) {
+            while (route.endsWith("/") || route.endsWith("?"))
+                route = route.substr(0, route.length - 1).trimRight();
+        }
+        return route;
+    }
     get pathArgs() {
         let routeArgs = this.__routeArgs;
         return routeArgs ? routeArgs.pathArgs : null;
