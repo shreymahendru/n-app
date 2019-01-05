08f8b8fe4c32c21b821e809c424f3334935e08d2
nivinjoseph <nivinjoseph@outlook.com>
2019-01-05 17:21:48 -0500

    preparing to publish new version

--- dist/services/navigation-service/default-navigation-service.d.ts
+++ dist/services/navigation-service/default-navigation-service.d.ts
@@ -5,13 +5,13 @@ export declare class DefaultNavigationService implements NavigationService {
     readonly currentRoutePath: string;
     readonly currentRouteHash: string;
     constructor(vueRouter: any);
-    navigate(route: string, params: Object, replaceHistory?: boolean): void;
+    navigate(route: string, params?: object | null, replaceHistory?: boolean): void;
     navigateBack(): void;
     navigateForward(): void;
     navigateSiteSameTab(url: string, replaceHistory?: boolean): void;
     navigateSiteNewTab(url: string): void;
-    navigateSitePostSameTab(url: string, value: Object): void;
-    navigateSitePostNewTab(url: string, value: Object): void;
+    navigateSitePostSameTab(url: string, value: object): void;
+    navigateSitePostNewTab(url: string, value: object): void;
     getSiteQueryParam(key: string): string;
     private createForm;
     private getHash;
