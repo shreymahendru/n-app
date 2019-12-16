daa995483673afe07012687a51305d80fd28057f
nivinjoseph <nivinjoseph@outlook.com>
2019-12-16 00:58:57 -0500

    preparing to publish new version

--- dist/core/client-app.js
+++ dist/core/client-app.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Vue = require("./../../vendor/vue.v2.5.21.js");
+const Vue = require("./../../vendor/vue.v2.6.11.js");
 exports.Vue = Vue;
-const VueRouter = require("./../../vendor/vue-router.v3.0.2.js");
+const VueRouter = require("./../../vendor/vue-router.v3.1.3.js");
 Vue.use(VueRouter);
 const n_defensive_1 = require("@nivinjoseph/n-defensive");
 require("@nivinjoseph/n-ext");
