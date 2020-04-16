96ba9a712c61377e8f5fd43ecec328650d48c27c
nivinjoseph <nivinjoseph@outlook.com>
2020-04-16 15:23:46 -0400

    preparing to publish new version

--- dist/loaders/xx-raster-image-loader.js
+++ dist/loaders/xx-raster-image-loader.js
@@ -5,6 +5,7 @@ const Sharp = require("sharp");
 const loaderUtils = require("loader-utils");
 const Path = require("path");
 const n_util_1 = require("@nivinjoseph/n-util");
+const n_config_1 = require("@nivinjoseph/n-config");
 const imagemin = require("imagemin");
 function resize(filePath, width, height) {
     const promise = new Promise((resolve, reject) => {
@@ -39,18 +40,19 @@ function default_1(content) {
     const { width, height } = parsedResourceQuery;
     const options = loaderUtils.getOptions(this) || {};
     const context = options.context || this.rootContext;
+    const isDev = n_config_1.ConfigurationManager.getConfig("env") === "dev";
     const callback = this.async();
     const plugins = [
         require("imagemin-gifsicle")({}),
         require("imagemin-mozjpeg")({}),
-        require("imagemin-pngquant")({}),
+        require("imagemin-optipng")({}),
     ];
     if (width || height) {
         resize(this.resourcePath, width, height)
             .then(resized => {
             imagemin.buffer(resized.data, { plugins })
                 .then((data) => {
-                const url = loaderUtils.interpolateName(this, `[contenthash].${resized.ext}`, {
+                const url = loaderUtils.interpolateName(this, `[contenthash]${isDev ? "" : n_util_1.Uuid.create()}.${resized.ext}`, {
                     context,
                     content: data
                 });
@@ -81,4 +83,4 @@ function default_1(content) {
 }
 exports.default = default_1;
 exports.raw = true;
-//# sourceMappingURL=raster-image-loader.js.map
+//# sourceMappingURL=xx-raster-image-loader.js.map
