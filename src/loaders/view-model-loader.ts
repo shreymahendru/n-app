8cee3430ce12ce7abe305c8a7c1621907aa83bc3
nivinjoseph <nivinjoseph@outlook.com>
2021-04-27 09:47:57 -0400

    made view-model-loader compatible with both ts and js files

--- src/loaders/view-model-loader.ts
+++ src/loaders/view-model-loader.ts
@@ -41,7 +41,9 @@ export default function (content: string)
     
     // this.addDependency(dirPath + "/" + fileName.replace("-view-model.js", "-view.html"));
     
-    const className = fileName.replace(".ts", "").split("-").map(t => `${t[0].toUpperCase()}${t.substring(1)}`).join("");
+    const isJs = fileName.endsWith(".js");
+    
+    const className = fileName.replace(isJs ? ".js" : ".ts", "").split("-").map(t => `${t[0].toUpperCase()}${t.substring(1)}`).join("");
     // console.warn(className);
     // console.log(content);
     
@@ -57,7 +59,7 @@ export default function (content: string)
     if (ConfigurationManager.getConfig("env") !== "dev")
         return content;
     
-    const viewFileName = fileName.replace("-view-model.ts", "-view.html");
+    const viewFileName = fileName.replace(isJs ? "-view-model.js" : "-view-model.ts", "-view.html");
     const relativeViewFilePath = "." + Path.sep + viewFileName;
     // const relativeViewFilePath = relativeFilePath.substr(0, relativeFilePath.length - "-view-model.js".length) + "-view.html";
     
