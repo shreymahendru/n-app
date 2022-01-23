d044f9d9700455e81043e3ab3c881dcdf2fef8e0
nivinjoseph <nivinjoseph@outlook.com>
2022-01-23 11:37:48 -0500

    preparing to publish new version

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.resolve = exports.DisplayType = exports.Utils = exports.debounce = exports.PageViewModel = exports.ComponentViewModel = exports.persist = exports.pages = exports.components = exports.events = exports.bind = exports.meta = exports.title = exports.template = exports.route = exports.element = exports.ClientApp = exports.Vue = void 0;
+require("@nivinjoseph/n-ext");
 const client_app_1 = require("./core/client-app");
 Object.defineProperty(exports, "Vue", { enumerable: true, get: function () { return client_app_1.Vue; } });
 Object.defineProperty(exports, "ClientApp", { enumerable: true, get: function () { return client_app_1.ClientApp; } });
