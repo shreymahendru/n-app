3222e119ef5e047f6263990f0c7fae2804ead67e
shreymahendru <shreymahendru@gmail.com>
2024-08-07 00:05:53 -0400

    change

--- vite.config.ts
+++ vite.config.ts
@@ -128,7 +128,7 @@ export default defineConfig({
                 changeOrigin: true,
                 secure: false,
                 selfHandleResponse: true,
-                configure: (proxy, opt) =>
+                configure: (proxy, _opt) =>
                 {
 
                     // proxy.on("")
