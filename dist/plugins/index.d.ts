c6a5c7acc870698de6cfb72a09e39f609e6bbe18
shreymahendru <shreymahendru@gmail.com>
2024-08-08 02:46:42 -0400

    dist

--- dist/plugins/index.d.ts
+++ dist/plugins/index.d.ts
@@ -1,4 +1,5 @@
 import { ViteNAppBabelPlugin } from "./vite-n-app-babel-plugin.js";
+import { ViteNAppRequirePlugin } from "./vite-n-app-require-plugin.js";
 import { ViteNAppViewPlugin } from "./vite-n-app-view-plugin.js";
-export { ViteNAppBabelPlugin, ViteNAppViewPlugin };
+export { ViteNAppBabelPlugin, ViteNAppViewPlugin, ViteNAppRequirePlugin };
 //# sourceMappingURL=index.d.ts.map
---
+++ dist/plugins/vite-n-app-require-plugin.d.ts
@@ -0,0 +1,6 @@
+import type { Plugin } from "vite";
+export declare function ViteNAppRequirePlugin(opts?: {
+    fileRegex?: RegExp;
+    translateType?: "importMetaUrl" | "import";
+}): Plugin;
+//# sourceMappingURL=vite-n-app-require-plugin.d.ts.map
